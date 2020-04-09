82a7811b9f1484696f8c0c60b04f431e092b8735
freshmaru <63366874+freshmaru@users.noreply.github.com>
2020-04-10 04:56:14 +0900

    Update padding.c

--- padding.c
+++ padding.c
@@ -10,7 +10,7 @@ int main()
     printf("-----Kitak Oh   2017038035-----\n\n");
 
     struct student pst;
-    printf("size of student = %ld\n", sizeof(struct student));      //studet구조체의 크기 출력 (padding된 크기 출력)
+    printf("size of student = %ld\n", sizeof(struct student));      //student구조체의 크기 출력 (padding된 크기 출력)
     printf("size of int = %ld\n", sizeof(int));                     //int의 크기 출력
     printf("size of short = %ld\n", sizeof(short));                 //short의 크기 출력
 
